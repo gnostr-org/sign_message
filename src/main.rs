268a1db83244d407bf4a3dfa9672140d2937d84e
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-03 11:40:17 -0500

    src/main.rs:restore

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+///https://docs.rs/secp256k1/latest/secp256k1/struct.Keypair.html#impl-Keypair
 fn main() {
     use secp256k1::{Keypair, Secp256k1, SecretKey};
     use std::str::FromStr;
-
     let secp = Secp256k1::new();
-    let key =
-        SecretKey::from_str("0000000000000000000000000000000000000000000000000000000000000001")
-            .unwrap();
-    let key_pair = Keypair::from_secret_key(&secp, &key);
 
+    let private_key_arg = std::env::args()
+        .nth(1)
+        .expect("Missing private key argument");
+    let key = SecretKey::from_str(&private_key_arg).unwrap();
+
+    #[cfg(debug_assertions)]
+    //sign_message 0000000000000000000000000000000000000000000000000000000000000001
     assert_eq!(
         "0000000000000000000000000000000000000000000000000000000000000001",
         format!("{}", key.display_secret())
     );
+
+    let key_pair = Keypair::from_secret_key(&secp, &key);
+
+    #[cfg(debug_assertions)]
     assert_eq!(
         "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
         format!("{}", key_pair.public_key())
