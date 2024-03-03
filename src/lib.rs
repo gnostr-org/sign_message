59d7057ed35281f4d3cf697218d96ac7836f3868
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-03 11:00:52 -0500

    src/lib.rs:#[cfg(feature = "integration_tests")]

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[cfg(feature = "integration_tests")]
+    fn integration_test() {
+        // This test only runs when the "integration_tests" feature is enabled
+    }
+}
